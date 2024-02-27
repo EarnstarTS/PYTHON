07007fe8384182e211a5bb70477b5fc77a64c590
EarnstarTS <technology.earnstar.co.ke>
2024-02-27 09:00:20 +0300

    first commit

---
+++ user_input.py.py
@@ -0,0 +1,5 @@
+name = input('Enter first name: ')
+age = input('How old are you: ')
+location = input('Where are you located: ')
+
+print("Hello ", name, "you are ", age, "years old, and you live in ", location)
